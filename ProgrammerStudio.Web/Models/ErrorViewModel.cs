d6ed7ce2e15de9fa0ea3739b28a91b345d5509de
brenovandall <vbreno335@gmail.com>
2023-11-13 21:57:14 -0300

    Adicionar arquivos de projeto.

---
+++ ProgrammerStudio.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProgrammerStudio.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
