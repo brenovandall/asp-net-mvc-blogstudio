691ea0d0dc212fa636ccd62ee281c8e6e3cc21cb
brenovandall <vbreno335@gmail.com>
2023-11-25 19:18:47 -0300

    implementing tailwind updates

---
+++ ProgrammerStudio.Web/wwwroot/js/main.js
@@ -0,0 +1,20 @@
+﻿
+
+
+console.log("trueee")
+document.addEventListener("DOMContentLoaded", function () {
+    var dropdownButton = document.querySelector(".dropdownButton");
+    var dropdownMenu = document.querySelector(".dropdownMenu");
+
+dropdownButton.addEventListener("click", function () {
+    // Toggle the 'hidden' class to show/hide the dropdown menu
+    dropdownMenu.classList.toggle("hidden");
+        });
+
+// Close the dropdown if the user clicks outside of it
+document.addEventListener("click", function (event) {
+            if (!event.target.closest(".dropdownMenu") && !event.target.closest(".dropdownButton")) {
+    dropdownMenu.classList.add("hidden");
+            }
+        });
+    });
