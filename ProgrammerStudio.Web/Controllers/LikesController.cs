5c5faff1a73c0ec3fe051631e2587b9f3d4a99f3
brenovandall <vbreno335@gmail.com>
2023-12-14 21:40:01 -0300

    adding like functionality

---
+++ ProgrammerStudio.Web/Controllers/LikesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProgrammerStudio.Web.Data;
+using ProgrammerStudio.Web.Models.Domain;
+using ProgrammerStudio.Web.Models.ViewModels;
+using ProgrammerStudio.Web.Service;
+
+namespace ProgrammerStudio.Web.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LikesController : ControllerBase
+{
+
+    private ApplicationDbContext _context;
+    private readonly IGetAllTheLikes _getAllTheLikes;
+
+    public LikesController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    [Route("Likes")]
+    public async Task<IActionResult> AddLike([FromBody] AddLikeViewModel addLikeViewModel)
+    {
+
+        var like = new BlogPostLike()
+        {
+            PostId = addLikeViewModel.BlogId,
+            UserId = addLikeViewModel.UserId
+        };
+
+        await _context.Likes.AddAsync(like);
+        await _context.SaveChangesAsync();
+
+        return Ok();
+
+    }
+
+    [HttpGet]
+    [Route("{BlogId:Guid}/totallikes")]
+    public async Task<IActionResult> GetTotalLikes([FromRoute] Guid postId)
+    {
+        var totalLikes = await _getAllTheLikes.GetAllTheLikesOfThePost(postId);
+
+        return Ok(totalLikes);
+    }
+}
---
+++ ProgrammerStudio.Web/Models/ViewModels/AddLikeViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ProgrammerStudio.Web.Models.ViewModels;
+
+public class AddLikeViewModel
+{
+    public Guid BlogId { get; set; }
+    public Guid UserId { get; set; }
+}
