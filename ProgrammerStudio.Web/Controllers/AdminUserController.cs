ea35bbb2c6e21c835108f663d8b62e115c6002d0
brenovandall <vbreno335@gmail.com>
2023-12-19 21:51:19 -0300

    displaying all the users signed to the super admin

---
+++ ProgrammerStudio.Web/Controllers/AdminUserController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProgrammerStudio.Web.Data;
+using ProgrammerStudio.Web.Models.ViewModels;
+
+namespace ProgrammerStudio.Web.Controllers;
+
+[Authorize(Roles = "superAdmin")]
+public class AdminUserController : Controller
+{
+    private readonly AuthDbContext _authcontext;
+    private UserManager<IdentityUser> _userManager;
+
+    public AdminUserController(AuthDbContext context)
+    {
+        _authcontext = context;
+    }
+
+    public async Task<IActionResult> List()
+    {
+        var users = _authcontext.Users.ToList();
+
+        var userAdminLogged = "37fcd00d-418a-453f-a4cb-e8b271c22d9b";
+
+        foreach(var user in users)
+        {
+            if(user.Id == userAdminLogged)
+            {
+                users.Remove(user);
+            }
+        }
+
+        var userViewModel = new UserViewModel();
+        userViewModel.Users = new List<User>();
+
+        foreach (var user in users)
+        {
+            userViewModel.Users.Add(new User
+            {
+                Id = Guid.Parse(user.Id),
+                Username = user.UserName,
+                Email = user.Email
+            });
+        }
+
+        return View(userViewModel);
+    }
+}
